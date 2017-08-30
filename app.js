b7b07b85312ce6f4283ea13561cf66910f6db307
EliGarcia <garciaelisha93@yahoo.com>
2017-08-30 11:42:14 -0500

    able to display homepage

--- app.js
+++ app.js
@@ -1,23 +1,59 @@
-const models = require("./models");
+const express = require('express'),
+  mustacheExpress = require('mustache-express'),
+  bodyParser = require('body-parser'),
+  sequelize = require('sequelize')
+models = require("./models");
 
-function createUser() {
-  const user = models.User.build({
-    name: 'Eli Garcia',
-    email: 'email@email.com',
-    bio: 'my bio'
-  });
+const app = express();
 
-user.save().then(function (newTodo) {
-  console.log(newTodo.id);
+app.engine('mustache', mustacheExpress());
+app.set('views', './views');
+app.set('view engine', 'mustache')
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+app.get('/', function(req, res) {
+  res.render("index");
 })
-}
-createUser();
 
-function listUsers() {
-models.User.findAll().then(function (users) {
-  users.forEach(function(user) {
-    console.log(user.dataValues)
-  })
-})
-}
-listUsers();
+app.listen(3000, function() {
+  console.log('Express running on http://localhost:3000/.')
+});
+
+process.on('SIGINT', function() {
+  console.log("\nshutting down");
+  const index = require('./models/index')
+  index.sequelize.close()
+
+  setTimeout(function() {
+    console.log('we are down Captain');
+    process.exit(0);
+  }, 500)
+});
+
+
+// const models = require("./models");
+//
+// function createUser() {
+//   const user = models.User.build({
+//     name: 'Eli Garcia',
+//     email: 'email@email.com',
+//     bio: 'my bio'
+//   });
+//
+// user.save().then(function (newTodo) {
+//   console.log(newTodo.id);
+// })
+// }
+// createUser();
+//
+// function listUsers() {
+// models.User.findAll().then(function (users) {
+//   users.forEach(function(user) {
+//     console.log(user.dataValues)
+//   })
+// })
+// }
+// listUsers();
